8b2d4cfbd3ddbc35cdbcd50e43f3a0ae04d74bb0
joe <joe@astucc.io>
2017-02-25 10:44:52 -0500

    Load a random quote each time the page is refreshed

--- random-quote-machine/quotes.js
+++ random-quote-machine/quotes.js
@@ -16,11 +16,61 @@ You can get feedback on your project by sharing it with your friends on Facebook
 
 */
 
-var view = {
+/* Fisher-Yates shuffle
+
+https://bost.ocks.org/mike/shuffle/
+	
+	function shuffle(array) {
+  var m = array.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
+
+*/
+
+
+
+
+var data = {
+	"quotes": [
+		{
+			"quote": "No such thing as spare time\nNo such thing as free time\nNo such thing as downtime\nAll you got is a lifetime\nGo",
+		  "author": "Henry Rollins"
+		},
+		{
+			"quote": "Live passionately, even if it kills you, because something is going to kill you anyway.",
+			"author": "Webb Chiles"
+		},
+		{
+			"quote": "A man is not old until regrets take the place of dreams",
+			"author": "John Barrymore"
+		}
+	]
+};
+
+var view = { 
 	displayQuotes: function() {
 		var quotesDiv = document.getElementById('quotes');
-		quotesDiv.innerHTML = 'test quote';
-	}
+		// for (var i = 0;)
+		quotesDiv.innerHTML = data.quotes[view.getRandomIntInclusiv(0,2)].quote;
+	},
+	getRandomIntInclusiv: function(min, max) {
+	  min = Math.ceil(min);
+	  max = Math.floor(max);
+	  return Math.floor(Math.random() * (max - min + 1));
+}
 }
 
 view.displayQuotes();
